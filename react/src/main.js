054413c9f7ac7a8510f4363fc1987fec3b0ce059
marclevetin <marc@derbyhat.org>
2017-04-28 18:10:25 -0400

    first commit

---
+++ react/src/main.js
@@ -0,0 +1,10 @@
+import 'babel-polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+$(function() {
+  ReactDOM.render(
+    <h1>My Ukulele dot Info - Main.js</h1>,
+    document.getElementById('app')
+  );
+});
