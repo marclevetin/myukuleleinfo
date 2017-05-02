65c90466c74c7fc4def4fa498ab1deaf06576d68
marclevetin <marc@derbyhat.org>
2017-05-02 13:35:01 -0400

    hound fixes

--- db/migrate/20170501225947_add_ukueleles_table.rb
+++ db/migrate/20170501225947_add_ukueleles_table.rb
@@ -8,34 +8,28 @@ class AddUkuelelesTable < ActiveRecord::Migration[5.0]
       t.string :model
       t.integer :serial_number
       t.date :purchase_date
-
       t.string :body_top
       t.string :body_sides
       t.string :body_back
       t.string :body_neck
       t.string :body_fretboard
       t.string :body_binding
-
       t.string :tuner_type
       t.string :tuner_manufacture
-
       t.string :string_manufacture
       t.integer :string_count
       t.string :string_tuning
-
       t.string :finish_type
       t.string :finish_look
-
       t.string :pickup
       t.string :nut_material
       t.string :saddle_material
-
       t.integer :neck_width
       t.integer :length_scale
       t.integer :length_body
       t.integer :width
       t.integer :depth
-
+      
       t.belongs_to :users
 
       t.timestamps null: false
