a4f440bfb1561cf00ecad03b2be1e205f0a24533
marclevetin <marc@derbyhat.org>
2017-05-04 19:50:03 -0400

    uke form added

--- app/controllers/ukuleles_controller.rb
+++ app/controllers/ukuleles_controller.rb
@@ -1,10 +1,36 @@
 class UkulelesController < ApplicationController
   def index
+    @ukuleles = Ukulele.all
   end
 
   def new
+    @ukulele = Ukulele.new
+    @user = current_user
   end
 
   def create
+    @user = current_user
+    @ukulele = Ukulele.create(ukulele_params)
+    @ukulele.user = @user
+
+    if @ukulele.save
+      flash[:notice] = "Ukulele saved"
+      redirect_to user_ukuleles_path(@user)
+    else
+      flash[:alert] = "Ukulele didn't save.  Try again."
+      render :new
+    end
   end
+
+  private
+
+  def ukulele_params
+    params.require(:ukulele).permit(
+      :instrument_type,
+      :ukulele_shape,
+      :ukulele_size,
+      :luthier
+    )
+  end
+
 end
