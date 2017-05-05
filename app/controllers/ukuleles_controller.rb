d990d555030a67f674b2e6eaf6faafe37b945be9
marclevetin <marc@derbyhat.org>
2017-05-05 01:10:29 -0400

    edit and update for ukulele working

--- app/controllers/ukuleles_controller.rb
+++ app/controllers/ukuleles_controller.rb
@@ -1,21 +1,5 @@
 class UkulelesController < ApplicationController
-  before_action :set_uke, only [:edit, :destroy]
-
-  def index
-    @ukuleles = Ukulele.all
-  end
-
-  def edit
-
-  end
-
-  def new
-    @ukulele = Ukulele.new
-    @user = current_user
-    @instrument_type = InstrumentType.all
-    @ukulele_shape = UkuleleShape.all
-    @ukulele_size = UkuleleSize.all
-  end
+  before_action :set_uke, only: [:edit, :destroy, :update]
 
   def create
     @user = current_user
@@ -31,21 +15,49 @@ class UkulelesController < ApplicationController
     end
   end
 
-  def show
-    @ukulele = Ukulele.find(params[:id])
-  end
-
   def destroy
     @ukulele.destroy
     respond_to do |format|
       format.html { redirect_to user_ukuleles_path(current_user), notice: 'Ukulele was successfully deleted.' }
-      format.json { head :no_content }
+    end
+  end
+
+  def edit
+    @instrument_type = InstrumentType.all
+    @ukulele_shape = UkuleleShape.all
+    @ukulele_size = UkuleleSize.all
+  end
+
+  def index
+    @ukuleles = Ukulele.all
+  end
+
+
+  def new
+    @ukulele = Ukulele.new
+    @user = current_user
+    @instrument_type = InstrumentType.all
+    @ukulele_shape = UkuleleShape.all
+    @ukulele_size = UkuleleSize.all
+  end
+
+  def show
+    @ukulele = Ukulele.find(params[:id])
+  end
+
+  def update
+    respond_to do |format|
+      if @ukulele.update(ukulele_params)
+        format.html { redirect_to user_ukuleles_path(current_user), notice: 'Ukulele was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
     end
   end
 
   private
   def set_uke
-    @ukelele = Ukulele.find(params[:id])
+    @ukulele = Ukulele.find(params[:id])
   end
 
   def ukulele_params
@@ -53,7 +65,31 @@ class UkulelesController < ApplicationController
       :instrument_type,
       :ukulele_shape,
       :ukulele_size,
-      :luthier
+      :luthier,
+      :model,
+      :serial_number,
+      :purchase_date,
+      :body_top,
+      :body_sides,
+      :body_back,
+      :body_neck,
+      :body_fretboard,
+      :body_binding,
+      :tuner_type,
+      :tuner_manufacture,
+      :string_manufacture,
+      :string_count,
+      :string_tuning,
+      :finish_type,
+      :finish_look,
+      :pickup,
+      :nut_material,
+      :saddle_material,
+      :neck_width,
+      :length_scale,
+      :length_body,
+      :width,
+      :depth
     )
   end
 
