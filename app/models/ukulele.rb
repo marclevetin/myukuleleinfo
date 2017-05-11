fff5c94dc6d605054103ca3e92e9a3b90e3c7351
marclevetin <marc@derbyhat.org>
2017-05-11 12:44:24 -0400

    fixed bug with Ukulele.youtube method.

--- app/models/ukulele.rb
+++ app/models/ukulele.rb
@@ -55,7 +55,7 @@ class Ukulele < ApplicationRecord
   end
 
   def youtube(url)
-    if !url.nil?
+    if !url.empty?
       uid_start = url.index("=") + 1
       uid_end = uid_start + 11
       url.slice(uid_start, uid_end)
