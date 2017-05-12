0dd28dbb7092daac8c01f2e89f7b3536a0b38aad
marclevetin <marc@derbyhat.org>
2017-05-11 22:34:03 -0400

    brainstormed list complete

---
+++ spec/features/ukulele_crud_spec.rb
@@ -0,0 +1,48 @@
+Add a ukulele
+  instrument type is required
+  shape is required
+  size is required
+  date is a date
+  dimensions are all numbers
+  photos are added
+  video is parsed correctly
+  show page has appropriate share code
+  Nickname appears where it's supposed to
+  Success message appears
+  Alert appears
+
+Edit a ukulele
+  See add ukulele
+  Success message appears
+  Alert appears
+
+Delete a ukulele
+  Uke is removed
+  Confirmation appears
+  Success message appears
+  Alert appears
+
+Other stuff
+  copy code button works - index
+  copy code button works - show
+  index share code shows correct content
+  link to Ukulele Underground works
+  Root path tkaes you to search page
+
+Show page
+  If you're not signed in, Add a video link should not appear
+  If you're not signed in, Add a photo link should not appear
+  Pictures section appears
+  Video section appears
+  Share section appears
+
+Navigation
+  Link to home page works
+  Sign in message is correct (Aloha)
+  My account goes to write page
+  Sign out link works
+  If you're not signed in, Register appears
+  If you're not signed in, Sign in appears
+
+Search page
+  Search results work
