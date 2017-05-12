5e91ca4b3c7260ba9e707a0c055c2ad6fe4b132e
marclevetin <marc@derbyhat.org>
2017-05-12 17:01:31 -0400

    more hound fixes

--- app/controllers/ukuleles_controller.rb
+++ app/controllers/ukuleles_controller.rb
@@ -61,10 +61,7 @@ class UkulelesController < ApplicationController
     @user = current_user
     respond_to do |format|
       if @ukulele.update(ukulele_params)
-        format.html {
-          redirect_to user_ukulele_path(@user, @ukulele),
-          notice: 'Ukulele was successfully updated.'
-        }
+        format.html { redirect_to user_ukulele_path(@user, @ukulele), notice: 'Ukulele was successfully updated.' }
       else
         format.html { render :edit }
       end
